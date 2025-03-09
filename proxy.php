5c80f4f2f0870312750ceed3dfd46116aa41b933
Muhosia <curtaintears@gmail.com>
2025-03-09 14:45:49 +0800

    Create proxy.php

---
+++ proxy.php
@@ -0,0 +1,14 @@
+<?php
+// 简易代理
+// 允许跨域
+header('Access-Control-Allow-Origin: *');
+header('Content-type: text/html; charset=utf-8');
+/**
+ * 等到资源加载完成（或者直接等到提示你刷新）后在控制台执行下方代码：
+ * 注意：192.168.50.50:82为我给提供的php地址，也可以直接配置nginx。主要是为了返回服务器列表
+ * 特别提醒：不要使用别人的代理，因为这里会接收到明文的账号以及TOKEN！！！！！
+ * window['serviceListdUrl'] = "http://192.168.50.50:82/server";
+ * window['setServiceListdUrl'] = "http://192.168.50.50:82/server";
+ * start();
+ */
+exit('{"login":[999,997,990],"serverlist":[{"name":"无天测试","serverlist":[{"originalSrvid":1,"srvid":1,"srvaddr":"121.41.123.129","id":1,"serverAlias":"s1","serverName":"登录试试","opentime":"2022-03-23 23:59:51","srvport":35317,"type":2,"pf":"yfbx"}]}]}');
